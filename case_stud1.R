67095c2ed7aa0da494a7b60d6c5d449cf9e47cdd
Briker <vbriker@nikonprecision.com>
2018-06-26 16:37:18 -0700

    update

--- case_stud1.R
+++ case_stud1.R
@@ -5,9 +5,10 @@ library('rstudioapi')
 
 current_path = rstudioapi::getActiveDocumentContext()$path 
 setwd(dirname(current_path ))
-print( getwd() )
 
 #setwd("F:\\My_Docs\\univer\\DataScience\\Case_Studies\\Case Study 1\\casestudy1")
+
+print( getwd())
 
 df_beer = read.csv("Beers.csv",header = TRUE)
 df_brew = read.csv("Breweries.csv",header = TRUE)
@@ -25,20 +26,22 @@ BrewByState
 final_brew=merge(df_beer,df_brew,by="BrewID")
 head(final_brew)
 tail(final_brew)
+
 #Answer Q3
 na_count=sapply(final_brew, function(y) sum(length(which(is.na(y)))))
 na_count
 
-BrewByState_all = group_by(final_brew,final_brew$State,na.rm=TRUE) 
-BrewByState_ABV=summarize(BrewByState_all,Median = median(ABV, na.rm = T)  )
-BrewByState_IBU=summarize(BrewByState_all,Median = median(IBU, na.rm = T)  )
-
 #Answer Q4
-BrewByState_ABV
-BrewByState_IBU
+BrewByState_all = group_by(final_brew,final_brew$State,na.rm=TRUE) 
+
+BrewByState_ABV=as_data_frame(summarize(BrewByState_all,Median = median(ABV, na.rm = T) ,Max = max(ABV, na.rm = T)))
+BrewByState_IBU=as_data_frame(summarize(BrewByState_all,Median = median(IBU, na.rm = T) ,Max = max(IBU, na.rm = T)))
 
 colnames(BrewByState_ABV)[grep(".*State.*$", colnames(BrewByState_ABV))]='State'
 colnames(BrewByState_IBU)[grep(".*State.*$", colnames(BrewByState_IBU))]='State'
+
+select(BrewByState_ABV,State,Median)
+select(BrewByState_IBU,State,Median)
 
 par(las=2)
 barplot(BrewByState_ABV$Median,names.arg=BrewByState_ABV$State,
@@ -50,20 +53,26 @@ barplot(BrewByState_IBU$Median,names.arg=BrewByState_IBU$State,
         main = "Beer Median IBU by state",
         horiz=FALSE,col='darkblue',space=1)
 
+#Answer Q5
+#BrewByState_ABV_m=na.omit(BrewByState_ABV)
+#BrewByState_IBU_m=na.omit(BrewByState_IBU)
 
-BrewByState_ABV=summarize(BrewByState_all,Max = max(ABV, na.rm = T)  )
-BrewByState_IBU=summarize(BrewByState_all,Max = max(IBU, na.rm = T)  )
-BrewByState_ABV
-BrewByState_IBU
+#BrewByState_ABV_max=summarize(BrewByState_all,Max = max(ABV, na.rm = T)  )
+#BrewByState_IBU_max=summarize(BrewByState_all,Max = max(IBU, na.rm = T)  )
+
+#colnames(BrewByState_ABV)[grep(".*State.*$", colnames(BrewByState_ABV))]='State'
+#colnames(BrewByState_IBU)[grep(".*State.*$", colnames(BrewByState_IBU))]='State'
+
+
 #BrewByState_ABV=na.omit(BrewByState_ABV)
 #BrewByState_IBU=na.omit(BrewByState_IBU)
 
-colnames(BrewByState_ABV)[grep(".*State.*$", colnames(BrewByState_ABV))]='State'
-colnames(BrewByState_IBU)[grep(".*State.*$", colnames(BrewByState_IBU))]='State'
 
-#Answer Q5
-BrewByState_ABV$State[which(BrewByState_ABV$Max == max(BrewByState_ABV$Max))[1]]
-BrewByState_IBU$State[which(BrewByState_IBU$Max == max(BrewByState_IBU$Max))[1]]
+BrewByState_ABV$State[which(BrewByState_ABV$Max == max(BrewByState_ABV$Max))]
+BrewByState_IBU$State[which(BrewByState_IBU$Max == max(BrewByState_IBU$Max))]
+
+#BrewByState_ABV
+#BrewByState_IBU
 
 #Answer Q6
 summary(final_brew$ABV)
